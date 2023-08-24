bd37359e8f953c0321e4853204de554b3cfb48b5
iceice666 <iceice666@outlook.com>
2023-08-24 23:23:25 +0800

    add ptt calculation

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,106 @@
 mod score;
-mod song;
 
 use std::fs;
 
 use anyhow::Result as anyResult;
 use score::get_score;
-use song::get_song;
+use score::update_info;
 
 fn main() -> anyResult<()> {
     // check file exist
     if !fs::metadata("arcsong.db").is_ok() {
-        panic!(
-            "Song database ( called `arcsong.db` ) do not exist! 
-You can download it at https://github.com/Arcaea-Infinity/ArcaeaSongDatabase"
-        )
-    }
-    if !fs::metadata("score.db").is_ok() {
-        panic!(
-            "Score database ( called `score.db` ) do not exist! 
-You can download it at https://github.com/Arcaea-Infinity/ArcaeaSongDatabase"
-        )
-    }
-
-    // Get local score data
-    let score_data = get_score()?;
-
-    let song_data: Vec<(String, u8)> = score_data
-        .iter()
-        .map(|score| (score.song_id.clone(), score.song_difficulty))
-        .collect();
-
-    let song_data = get_song(Some(song_data))?;
-
-    for score in score_data.iter() {
-        let song = song_data.get(&format!("{}-{}", score.song_id, score.song_difficulty));
         println!(
-            "{} {:8} {} \n Perfect: {} (+{}) Far: {} Lost: {}\n",
-            {
-                match song {
-                    Some(_) => match &song.unwrap().name_jp {
-                        Some(v) => v,
-                        None => &song.unwrap().name_en,
-                    },
-                    None => &score.song_id,
-                }
-            },
-            score.score,
-            {
-                match song {
-                    Some(_) => format!(
-                        "{:.2} -> {:.2}",
-                        song.unwrap().rating as f64 / 10.0,
-                        (song.unwrap().rating as f64 + {
-                            if score.score >= 10_000_000 {
-                                20 as f64
-                            } else if score.score >= 9_800_000 {
-                                (score.score - 9800000 + 200000) as f64 / 20000 as f64
-                            } else {
-                                (score.score as i64 - 9500000) as f64 / 30000 as f64
-                            }
-                        }) * 0.1
-                    ),
-
-                    None => "".to_string(),
-                }
-            },
-            score.perfect_count,
-            score.max_perfect_count,
-            score.far_count,
-            score.lost_count
+            "Song database ( called `arcsong.db` ) do not exist! 
+PTT calculation is tempeorarily disabled.
+You can download it at https://github.com/Arcaea-Infinity/ArcaeaSongDatabase"
         )
     }
+
+    let mut data_path = "score.db";
+
+    if fs::metadata("/data/data/moe.low.arc/files/st3").is_ok() {
+        data_path = "/data/data/moe.low.arc/files/st3";
+    } else {
+        if !fs::metadata("score.db").is_ok() {
+            panic!(
+                "Score database ( called `score.db` ) do not exist! 
+You have to copy it at /data/data/moe.low.arc/files/st3 ( and you will need your device be rooted).
+And rename to `score.db`."
+            )
+        }
+    }
+
+    let score_data = get_score(data_path)?;
+    let mut song_data = update_info(score_data)?;
+
+    song_data.sort_by(|a, b| {
+        let mut ptt_a: f64 = -1.0;
+        let mut ptt_b: f64 = -1.0;
+
+        if let Some(v) = &a.info {
+            ptt_a = (v.rating as f64 + a.ptt_offset) * 0.1;
+        }
+
+        if let Some(v) = &b.info {
+            ptt_b = (v.rating as f64 + b.ptt_offset) * 0.1;
+        }
+        ptt_b.partial_cmp(&ptt_a).unwrap()
+    });
+
+    let mut ptt: f64 = 0.0;
+    for i in 0..30 {
+        let song = &song_data[i];
+
+        print!("#{}. ", i + 1);
+
+        match &song.info {
+            None => println!(
+                "{} {} {}",
+                song.song_id,
+                {
+                    match song.song_difficulty {
+                        0 => "PST",
+                        1 => "PRS",
+                        2 => "FTR",
+                        3 => "BYD",
+                        _ => "???",
+                    }
+                },
+                song.score
+            ),
+            Some(info) => {
+                ptt += format!("{:.4}", (info.rating as f64 + song.ptt_offset) * 0.1)
+                    .parse::<f64>()?;
+
+                println!(
+                    "{} [{}] \n{:08} {:.2} -> {:.4}",
+                    match &info.name_jp {
+                        None => &info.name_en,
+                        Some(v) => &v,
+                    },
+                    {
+                        match song.song_difficulty {
+                            0 => "PST",
+                            1 => "PRS",
+                            2 => "FTR",
+                            3 => "BYD",
+                            _ => "???",
+                        }
+                    },
+                    song.score,
+                    info.rating as f32 * 0.1,
+                    (info.rating as f64 + song.ptt_offset) * 0.1
+                )
+            }
+        }
+        println!(
+            "Perfect: {:4} (+{:4}) Far: {:4} Lost: {:4}",
+            song.perfect_count, song.max_perfect_count, song.far_count, song.lost_count,
+        );
+        println!();
+    }
+
+    println!("B30 AVG: {:.4}", ptt / 30.0);
 
     Ok(())
 }
--- src/score.rs
+++ src/score.rs
@@ -1,9 +1,10 @@
+use std::collections::HashMap;
+
 use anyhow::Result as anyResult;
 
-// field by arcaea's database
 #[derive(Debug)]
 pub struct Score {
-    // Just score 
+    // Just score
     pub score: u32, // 0 ~ 10,002,221
     // perfect count in +- 25ms. (Original called `shinyPerfectCount`)
     pub max_perfect_count: u16, // 0 ~ 2221
@@ -19,18 +20,23 @@ pub struct Score {
     pub song_difficulty: u8, // 0 ~ 3
     // remains health on clear
     pub health: i8, // ~ 100, negative value unknown right now
+    //calculated ptt
+    pub ptt_offset: f64,
+    // song's info
+    pub info: Option<Song>,
 }
 
-pub fn get_score() -> anyResult<Vec<Score>> {
-    let connection = sqlite::open("score.db")?;
+pub fn get_score(path: &str) -> anyResult<Vec<Score>> {
+    let connection = sqlite::open(path)?;
 
     let mut rows: Vec<Score> = vec![];
 
     let mut statement = connection.prepare("SELECT * FROM scores;")?;
 
     while let Ok(sqlite::State::Row) = statement.next() {
+        let score = statement.read::<i64, _>("score")?.try_into()?;
         rows.push(Score {
-            score: statement.read::<i64, _>("score")?.try_into()?,
+            score,
             max_perfect_count: statement.read::<i64, _>("shinyPerfectCount")?.try_into()?,
             perfect_count: statement.read::<i64, _>("perfectCount")?.try_into()?,
             far_count: statement.read::<i64, _>("nearCount")?.try_into()?,
@@ -38,10 +44,84 @@ pub fn get_score() -> anyResult<Vec<Score>> {
             song_id: statement.read::<String, _>("songId")?,
             song_difficulty: statement.read::<i64, _>("songDifficulty")?.try_into()?,
             health: statement.read::<i64, _>("health")?.try_into()?,
+            ptt_offset: {
+                if score >= 10_000_000 {
+                    20 as f64
+                } else if score >= 9_800_000 {
+                    (score - 9800000 + 200000) as f64 / 20000 as f64
+                } else {
+                    (score as i64 - 9500000) as f64 / 30000 as f64
+                }
+            },
+            info: None,
         })
     }
 
     Ok(rows)
+}
+
+#[derive(Debug)]
+pub struct Song {
+    pub song_id: String,
+    pub name_en: String,
+    pub name_jp: Option<String>,
+    pub rating_class: u8,
+    pub rating: u8,
+}
+
+/// It takes a Vec<Score> as input, update items' [Score::info] value.
+pub fn update_info(mut scores: Vec<Score>) -> anyResult<Vec<Score>> {
+    // setup database
+    let connection = sqlite::open("arcsong.db")?;
+
+    // create SQL expression
+    let mut query = ("SELECT * FROM charts WHERE 0 ").to_string();
+    for score in scores.iter() {
+        query.push_str(&format!(
+            "OR ( song_id = '{}' AND rating_class = {} )",
+            score.song_id, score.song_difficulty
+        ))
+    }
+
+    // create a map to save song info
+    let mut map: HashMap<String, Song> = HashMap::new();
+
+    // prepare to iter through database
+    let mut statement = connection.prepare(query)?;
+
+    // iteration
+    while let Ok(sqlite::State::Row) = statement.next() {
+        let song_id = statement.read::<String, _>("song_id")?;
+        let name_en = statement.read::<String, _>("name_en")?;
+        let name_jp = {
+            let t = statement.read::<String, _>("name_jp")?;
+            if t == "" {
+                None
+            } else {
+                Some(t)
+            }
+        };
+        let rating_class = statement.read::<i64, _>("rating_class")?.try_into()?;
+        let rating = statement.read::<i64, _>("rating")?.try_into()?;
+
+        map.insert(
+            format!("{}-{}", song_id.clone(), rating_class),
+            Song {
+                song_id,
+                name_en,
+                name_jp,
+                rating_class,
+                rating,
+            },
+        );
+    }
+
+    // Update each item's [Score::info]
+    for score in scores.iter_mut() {
+        score.info = map.remove(&format!("{}-{}", score.song_id, score.song_difficulty));
+    }
+
+    Ok(scores)
 }
 
 #[cfg(test)]
@@ -51,13 +131,14 @@ mod tests {
 
     #[test]
     fn test_get_score() -> anyResult<()> {
-        let result = get_score()?;
+        let result = get_score("score.db")?;
         println!(
             "        song         |  score   |   perfect   | far  | lost | health | difficulty "
         );
         println!(
             "---------------------|----------|-------------|------|------|--------|------------"
         );
+
         for score in result.iter() {
             println!(
                 "{:20} | {:8} | {:11} | {:4} | {:4} | {:6} | {:3}",
@@ -82,6 +163,75 @@ mod tests {
                     }
                 }
             );
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_update_info() -> anyResult<()> {
+        let score_data = get_score("score.db")?;
+        let mut song_data = update_info(score_data)?;
+
+        song_data.sort_by(|a, b| {
+            let mut ptt_a: f64 = -1.0;
+            let mut ptt_b: f64 = -1.0;
+
+            if let Some(v) = &a.info {
+                ptt_a = (v.rating as f64 + a.ptt_offset) * 0.1;
+            }
+
+            if let Some(v) = &b.info {
+                ptt_b = (v.rating as f64 + b.ptt_offset) * 0.1;
+            }
+            ptt_b.partial_cmp(&ptt_a).unwrap()
+        });
+
+        for i in 0..30 {
+            let song = &song_data[i];
+
+            print!("#{}. ", i + 1);
+
+            match &song.info {
+                None => println!(
+                    "{} {} {}",
+                    song.song_id,
+                    {
+                        match song.song_difficulty {
+                            0 => "PST",
+                            1 => "PRS",
+                            2 => "FTR",
+                            3 => "BYD",
+                            _ => "???",
+                        }
+                    },
+                    song.score
+                ),
+                Some(info) => println!(
+                    "{} [{}] {:8} {:.2} -> {:.2}",
+                    match &info.name_jp {
+                        None => &info.name_en,
+                        Some(v) => &v,
+                    },
+                    {
+                        match song.song_difficulty {
+                            0 => "PST",
+                            1 => "PRS",
+                            2 => "FTR",
+                            3 => "BYD",
+                            _ => "???",
+                        }
+                    },
+                    song.score,
+                    info.rating as f32 * 0.1,
+                    (info.rating as f64 + song.ptt_offset) * 0.1
+                ),
+            }
+            println!(
+                "Perfect: {} (+{}) Far: {} Lost: {}",
+                song.perfect_count, song.max_perfect_count, song.far_count, song.lost_count,
+            );
+            println!();
         }
 
         Ok(())
